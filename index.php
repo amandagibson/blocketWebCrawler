aeeee0269732afa1cb7562ef86b8e23af1756923
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-31 18:27:45 +0200

    pulls titles from all links

--- index.php
+++ index.php
@@ -5,6 +5,19 @@ $start = "https://www.blocket.se/goteborg?ca=15";
 $already_crawled = array();
 
 function get_details($url)  {
+  $options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: amandaBot/0.1\n"));
+
+  $context = stream_context_create($options);
+
+  $doc = new DOMdocument('1.0', 'UTF-8');
+  $internalErrors = libxml_use_internal_errors(true);
+  @$doc->loadHTML(@file_get_contents($url, false, $context));
+  libxml_use_internal_errors($internalErrors);
+
+  $title = $doc->getElementsByTagName("title");
+  $title = $title->item(0)->nodeValue;
+
+  echo $title."\n";
 
 }
 
@@ -27,8 +40,8 @@ function follow_links($url) {
 
     if (!in_array($l, $already_crawled)) {
       $already_crawled[] = $l;
-      //echo get_details($l);
-      echo $l."\n";
+      echo get_details($l);
+      //echo $l."\n";
 
     }
   }
