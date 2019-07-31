53f482c4ccada5db5711188023a0fdfe0df24ffb
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-31 18:15:56 +0200

    adds headers and changes User Agent

--- index.php
+++ index.php
@@ -4,13 +4,20 @@ $start = "https://www.blocket.se/goteborg?ca=15";
 
 $already_crawled = array();
 
-function follow_links($url) {
+function get_details($url)  {
 
+}
+
+function follow_links($url) {
   global $already_crawled;
+
+  $options = array('http'=>array('method'=>"GET", 'headers'=>"User-Agent: amandaBot/0.1\n"));
+
+  $context = stream_context_create($options);
 
   $doc = new DOMdocument('1.0', 'UTF-8');
   $internalErrors = libxml_use_internal_errors(true);
-  $doc->loadHTML(file_get_contents($url));
+  $doc->loadHTML(file_get_contents($url, false, $context));
   libxml_use_internal_errors($internalErrors);
 
   $linklist = $doc->getElementsByTagName("a");
@@ -20,6 +27,7 @@ function follow_links($url) {
 
     if (!in_array($l, $already_crawled)) {
       $already_crawled[] = $l;
+      //echo get_details($l);
       echo $l."\n";
 
     }
