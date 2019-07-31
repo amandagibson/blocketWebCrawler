40353fb1b602f9abb70a8f5eac789fbd52f13546
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-07-31 18:08:24 +0200

    adds links to an array so that we only crawl them once.

--- index.php
+++ index.php
@@ -2,7 +2,11 @@
 
 $start = "https://www.blocket.se/goteborg?ca=15";
 
+$already_crawled = array();
+
 function follow_links($url) {
+
+  global $already_crawled;
 
   $doc = new DOMdocument('1.0', 'UTF-8');
   $internalErrors = libxml_use_internal_errors(true);
@@ -12,9 +16,16 @@ function follow_links($url) {
   $linklist = $doc->getElementsByTagName("a");
 
   foreach ($linklist as $link) {
-    echo $link->getAttribute("href")."\n";
-  }
+    $l = $link->getAttribute("href");
 
+    if (!in_array($l, $already_crawled)) {
+      $already_crawled[] = $l;
+      echo $l."\n";
+
+    }
+  }
 }
 
 follow_links($start);
+
+print_r($already_crawled);
